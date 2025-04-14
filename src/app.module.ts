0603e72df852d12d1daa28c2de6b28febb487128
Jongun Park <azznggu@gmail.com>
2025-04-14 19:35:43 +0900

    feat: 프론트엔드 UI 구현 및 정적 파일 서빙 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [UsersModule, OrdersModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+    }),
+    UsersModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
